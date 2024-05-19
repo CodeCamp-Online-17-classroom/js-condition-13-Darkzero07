ef7a8ae49eaea37b1768544301d3f4dc168bddab
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 10:59:56 +0700

    js lab 30

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+let numInput = Number(prompt("Enter your number : "));
+switch (numInput) {
+    case 1: alert("One");
+    break;
+    case 0: alert("Zero");
+    break;
+    case -1: alert("Minus");
+    break;
+    default: alert("Invalid number")
+}
